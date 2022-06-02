76405e42b28039a731dfcc03a999be5c082dbefc
201255 <201255@ids.upchiapas.edu.mx>
2022-06-02 13:17:10 -0500

    third commit NodeJs

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,47 +1,123 @@
 const { query } = require('express');
 const express = require('express');
 
-const response= require('../../../network/response');
-
+const response = require('../../../network/response');
+const { getConnection } = require('../../../model/db');
+const { request } = require('express');
+var cors = require('cors')
 const router = express.Router();
 
-router.get('/', function(req, res){
-    
-    // res.send({
-    //     success: 'success 1',
-    // })
-    response.succes(req,res,'',200)
-    
-});
+var allowlist = ['http://localhost:3000', ''];
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } 
+    } else {
+      corsOptions = { origin: false } 
+    }
+    callback(null, corsOptions) 
+  }
 
-router.post('/login',function(req,res){
-    let username=req.query.username;
-    let password=req.query.password;
+router.get('/obtener',cors(corsOptionsDelegate), async function (req, res) {
+    const client = await getConnection();
+
+    let id = req.query.id
+
+    const query_request = {
+        text: 'SELECT * FROM tbl_usersdb ',
+        
+    }
+
+    client.query(query_request)
+        .then(r => { response.success(req, res, r, 200); })
+        .catch(e => { response.success(req, res, e.stack, 400); })
+})
+
+router.post('/login', function (req, res) {
+    let username = req.query.username;
+    let password = req.query.password;
     console.log(req.query);
     res.send({
         username,
         password,
-        token:'token',
-        id_user:'id_user',
-        success:'Ok'
-})
+        token: 'token',
+        id_user: 'id_user',
+        success: 'Ok'
+    })
 })
 
-router.post('/register', function(req,res){
-    let username=req.query.username;
-    let password=req.query.password;
-    let email=req.query.email;
-    let number_phone=req.query.number_phone;
+router.post('/register', function (req, res) {
+    let username = req.query.username;
+    let password = req.query.password;
+    let email = req.query.email;
+    let number_phone = req.query.number_phone;
     console.log(req.query);
     res.send({
         username,
         password,
         email,
         number_phone,
-        token:'token',
-        id_user:'id_user',
-        success:'Ok'
+        token: 'token',
+        id_user: 'id_user',
+        success: 'Ok'
     })
 })
+
+router.post('/register2', cors(corsOptionsDelegate),async function (req, res) {
+    const client = await getConnection();
+
+    let username = req.query.username;
+    let email = req.query.email;
+    let password = req.query.password;
+    let phone_number = req.query.phone_number;
+
+    const query_request = {
+        text: 'INSERT INTO tbl_usersdb(username,email,password,phone_number) VALUES($1, $2, $3, $4)',
+        values: [username, email, password, phone_number]
+    };
+
+    client.query(query_request)
+        .then(r => { response.success(req, res, r, 200); })
+        .catch(e => { response.success(req, res, e, 200); })
+
+});
+
+router.delete('/delete', cors(corsOptionsDelegate),async function (req, res) {
+    const client = await getConnection();
+
+
+    let id = req.query.id;
+
+
+    const query_request = {
+        text: `DELETE FROM tbl_usersdb WHERE id=${id} `,
+
+    };
+
+    client.query(query_request)
+        .then(r => { response.success(req, res, r, 200); })
+        .catch(e => { response.success(req, res, e, 400); })
+});
+
+router.put('/update',cors(corsOptionsDelegate), async function (req, res) {
+    const client = await getConnection();
+
+    let id = req.query.id;
+    let username = req.query.username;
+    let email = req.query.email;
+    let password = req.query.password;
+    let phone_number = req.query.phone_number;
+
+    const query_request = {
+        text: `UPDATE tbl_usersdb SET username=$1, email=$2, password=$3, phone_number=$4 WHERE id=${id}`,
+        values: [username, email, password, phone_number]
+
+    };
+
+    client.query(query_request)
+        .then(r => { response.success(req, res, r, 200); })
+        .catch(e => { response.success(req, res, e.stack, 400); })
+
+});
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ const express =  require('express');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
+const cors = require('cors');
 
 const app = express ();
 
-app.use('/api/user', user);
+app.use('/api_v1/user', user);
+
+app.use(cors({origin: true, credentials: true})); 
 
 app.listen(config.api.port, () =>{
     console.log('Api escuchando en el puerto =>', config.api.port);
